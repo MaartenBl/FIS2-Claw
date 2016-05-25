d37a558c9b316cb5a34dc7eb5ae26b4ea06f92dd
Maarten <lucmaar@hotmail.com>
2016-05-25 12:51:02 +0200

    First commit
    -Upload EV3
    -Upload Form1(Application)

---
+++ Example Lego Mindstorms Bluetooth/Example Lego Mindstorms Bluetooth/LegoMindstormsForm.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Example_Lego_Mindstorms_Bluetooth
+{
+    partial class LegoMindstormsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectionGroupBox = new System.Windows.Forms.GroupBox();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portListBox = new System.Windows.Forms.ListBox();
+            this.inputGroupBox = new System.Windows.Forms.GroupBox();
+            this.labelInstructions = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonBackward = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.groupBoxOutput = new System.Windows.Forms.GroupBox();
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.connectionGroupBox.SuspendLayout();
+            this.inputGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connectionGroupBox
+            // 
+            this.connectionGroupBox.Controls.Add(this.disconnectButton);
+            this.connectionGroupBox.Controls.Add(this.connectButton);
+            this.connectionGroupBox.Controls.Add(this.refreshButton);
+            this.connectionGroupBox.Controls.Add(this.label1);
+            this.connectionGroupBox.Controls.Add(this.portListBox);
+            this.connectionGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectionGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.connectionGroupBox.Name = "connectionGroupBox";
+            this.connectionGroupBox.Size = new System.Drawing.Size(406, 131);
+            this.connectionGroupBox.TabIndex = 4;
+            this.connectionGroupBox.TabStop = false;
+            this.connectionGroupBox.Text = "Connection";
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disconnectButton.Location = new System.Drawing.Point(275, 93);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(120, 23);
+            this.disconnectButton.TabIndex = 2;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(275, 64);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(120, 23);
+            this.connectButton.TabIndex = 8;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshButton.Location = new System.Drawing.Point(275, 35);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(120, 23);
+            this.refreshButton.TabIndex = 7;
+            this.refreshButton.Text = "Referesh ports";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Port for blutooth connection";
+            // 
+            // portListBox
+            // 
+            this.portListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portListBox.FormattingEnabled = true;
+            this.portListBox.Location = new System.Drawing.Point(9, 35);
+            this.portListBox.Name = "portListBox";
+            this.portListBox.Size = new System.Drawing.Size(260, 82);
+            this.portListBox.TabIndex = 5;
+            // 
+            // inputGroupBox
+            // 
+            this.inputGroupBox.Controls.Add(this.labelInstructions);
+            this.inputGroupBox.Controls.Add(this.inputTextBox);
+            this.inputGroupBox.Controls.Add(this.inputButton);
+            this.inputGroupBox.Enabled = false;
+            this.inputGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputGroupBox.Location = new System.Drawing.Point(12, 151);
+            this.inputGroupBox.Name = "inputGroupBox";
+            this.inputGroupBox.Size = new System.Drawing.Size(200, 244);
+            this.inputGroupBox.TabIndex = 5;
+            this.inputGroupBox.TabStop = false;
+            this.inputGroupBox.Text = "Input";
+            // 
+            // labelInstructions
+            // 
+            this.labelInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInstructions.Location = new System.Drawing.Point(10, 15);
+            this.labelInstructions.Name = "labelInstructions";
+            this.labelInstructions.Size = new System.Drawing.Size(184, 63);
+            this.labelInstructions.TabIndex = 4;
+            this.labelInstructions.Text = "Via this form you can send a message to the Brick. The message will be saved and " +
+    "can be retrieved via the Output form.";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(9, 81);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(184, 20);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // inputButton
+            // 
+            this.inputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputButton.Location = new System.Drawing.Point(9, 111);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(184, 23);
+            this.inputButton.TabIndex = 0;
+            this.inputButton.Text = "Send message";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonDown);
+            this.groupBox1.Controls.Add(this.buttonBackward);
+            this.groupBox1.Controls.Add(this.buttonForward);
+            this.groupBox1.Controls.Add(this.buttonUp);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(218, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 244);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Movement";
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(56, 156);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.Text = "↓";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonBackward
+            // 
+            this.buttonBackward.Location = new System.Drawing.Point(119, 111);
+            this.buttonBackward.Name = "buttonBackward";
+            this.buttonBackward.Size = new System.Drawing.Size(75, 23);
+            this.buttonBackward.TabIndex = 2;
+            this.buttonBackward.Text = "→";
+            this.buttonBackward.UseVisualStyleBackColor = true;
+            this.buttonBackward.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Location = new System.Drawing.Point(6, 111);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(75, 23);
+            this.buttonForward.TabIndex = 1;
+            this.buttonForward.Text = "←";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(56, 69);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 0;
+            this.buttonUp.Text = "↑";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBoxOutput
+            // 
+            this.groupBoxOutput.Controls.Add(this.button1);
+            this.groupBoxOutput.Controls.Add(this.listBoxOutput);
+            this.groupBoxOutput.Enabled = false;
+            this.groupBoxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxOutput.Location = new System.Drawing.Point(424, 151);
+            this.groupBoxOutput.Name = "groupBoxOutput";
+            this.groupBoxOutput.Size = new System.Drawing.Size(200, 244);
+            this.groupBoxOutput.TabIndex = 7;
+            this.groupBoxOutput.TabStop = false;
+            this.groupBoxOutput.Text = "Output";
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.Location = new System.Drawing.Point(3, 16);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(191, 186);
+            this.listBoxOutput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Update Status";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LegoMindstormsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 407);
+            this.Controls.Add(this.groupBoxOutput);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.connectionGroupBox);
+            this.Controls.Add(this.inputGroupBox);
+            this.Name = "LegoMindstormsForm";
+            this.Text = "Example Lego Mindstorms Bluetooth";
+            this.connectionGroupBox.ResumeLayout(false);
+            this.connectionGroupBox.PerformLayout();
+            this.inputGroupBox.ResumeLayout(false);
+            this.inputGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBoxOutput.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox connectionGroupBox;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox portListBox;
+        private System.Windows.Forms.GroupBox inputGroupBox;
+        private System.Windows.Forms.Label labelInstructions;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonBackward;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.GroupBox groupBoxOutput;
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Example Lego Mindstorms Bluetooth/Example Lego Mindstorms Bluetooth/LegoMindstormsForm.cs
@@ -0,0 +1,172 @@
+﻿using EV3MessengerLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_Lego_Mindstorms_Bluetooth
+{
+    public partial class LegoMindstormsForm : Form
+    {
+        private EV3Messenger messenger;
+
+        public LegoMindstormsForm()
+        {
+            InitializeComponent();
+            // Init application
+            messenger = new EV3Messenger();
+            fillSerialPortSelectionBoxWithAvailablePorts();
+            updateFormGUI();
+        }
+        
+        #region Connection form
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            fillSerialPortSelectionBoxWithAvailablePorts();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Check if a port has been selected
+            if (portListBox.SelectedIndex > -1)
+            {
+                // Get the selected port from the ListBox
+                string port = portListBox.SelectedItem.ToString().ToUpper();
+                // Try to connect with the Brick via the selected port
+                if (messenger.Connect(port))
+                {
+                    updateFormGUI();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to connect to serial port '" + port + "'.\n"
+                        + "Make sure your robot is connected to that serial port and try again.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a port for the bluetooth connection");
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            // Disconnect from the Brick
+            messenger.Disconnect();
+
+            updateFormGUI();
+        }
+
+        private void fillSerialPortSelectionBoxWithAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+
+            portListBox.Items.Clear();
+            foreach (String port in ports)
+            {
+                portListBox.Items.Add(port);
+            }
+        }
+
+        #endregion
+
+        #region Input & output form
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            string txtMsg = inputTextBox.Text;
+            // Make sure a message has been typed
+            if (!String.IsNullOrWhiteSpace(txtMsg))
+            {
+                // Send a message to the Brick with title: MESSAGE and the message
+                if (messenger.SendMessage("Movement", txtMsg))
+                {
+                    inputTextBox.Text = "";
+                    MessageBox.Show("Command send.");
+                }
+                else
+                {
+                    MessageBox.Show("Please try again.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please type a command in the above textbox for the Brick");
+            }
+        }
+
+
+        #endregion
+
+        #region GUI
+
+        private void updateFormGUI()
+        {
+            if (messenger.IsConnected)
+            {
+                refreshButton.Enabled = false;
+                connectButton.Enabled = false;
+
+                inputGroupBox.Enabled = true;
+                groupBox1.Enabled = true;
+                groupBoxOutput.Enabled = true;
+                disconnectButton.Enabled = true;
+
+            }
+            else
+            {
+                refreshButton.Enabled = true;
+                connectButton.Enabled = true;
+
+                inputGroupBox.Enabled = false;
+                disconnectButton.Enabled = false;
+            }
+        }
+
+        #endregion
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            messenger.SendMessage("Movement", "U");
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            messenger.SendMessage("Movement", "D");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            messenger.SendMessage("Movement", "F");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            messenger.SendMessage("Movement", "B");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            EV3Message message = messenger.ReadMessage();
+           
+            if (message != null)
+            {
+                listBoxOutput.Items.Add("Message: " + message.ValueAsText);
+                listBoxOutput.TopIndex = listBoxOutput.Items.Count - 1;
+            }
+            else
+            {
+                MessageBox.Show("No message recieved from the Brick");
+            }
+        }
+    }
+}
